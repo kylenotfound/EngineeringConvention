1c5c5a3c232494371b3a2b58ecfc6729d8d50d2d
Kyle Evangelisto <kyleevangelisto@gmail.com>
2021-10-18 15:31:45 -0400

    working ada hw

--- Ada/src/main.adb
+++ Ada/src/main.adb
@@ -1,23 +1,53 @@
-with Ada.Text_IO;
-
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
 
-   Count : Integer;
-   type convention is array(1..5, 1..Count) of Integer; --Count will be the count of lines
+   Count, Option, Credits : Integer;
+   type convention is array(1..100, 1..5) of Integer; --Count will be the count of lines
    list : convention;
+
+    function Schedule
+        return Boolean is
+    begin
+        for wsA in 1..Count loop
+            for wsB in 1..Count loop
+                if not (wsA = wsB) and list(wsA, 2) = list(wsB, 2) then
+                    if((list(wsA, 4) <= list(wsB, 5)) and (list(wsA, 4) >= list(wsB, 4))) then  --Workshop A starts before Workshop B ends and after Workshop B starts
+                        return true;
+                    end if;
+                
+                end if;
+                
+                
+            end loop;
+        end loop;
+        
+        return false;
+    end Schedule; 
 
 begin
 
-   Count := Integer'Value (Ada.Text_IO.Get_Line);
-   --Ada.Text_IO.Put_Line(Integer'Image(Count));--debug--
-
+   Get(Count);
+   Credits := 0;
+   
    --Load array with input
-   for i in list'Range loop
-      for j in list'Range loop
-         Ada.Text_IO.Put_Line(Integer'Image(j));
+   for i in 1..Count loop
+      for j in 1..5 loop
+         Get(Option);
+         list(i,j) := Option;
       end loop;
-      Ada.Text_IO.Put_Line("--------------------");
    end loop;
-
+   
+   for i in 1..Count loop
+      Credits := Credits + list(i, 3);
+   end loop;
+      Put(Credits);
+      
+      Put_Line("");
+    if Credits < 5 or Schedule then
+        Put_Line ("No certificate");
+    else
+        Put_Line ("Certificate earned");
+    end if;
 
 end Main;
