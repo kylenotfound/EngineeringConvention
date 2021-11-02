c87b1e6cfc157e3a78375edbb32eb895569fa35b
Kyle Evangelisto <kyleevangelisto@gmail.com>
2021-11-01 21:07:02 -0400

    when 12.5 % of your grade is 20 lines bruhhhh

---
+++ Python/homework.py
@@ -0,0 +1,20 @@
+def certificate(Workshop_List):
+  if(checkCredits(Workshop_List) and checkDates(Workshop_List)) :
+    print("True")
+  else:
+    print("False")
+
+def checkDates(Workshop_List):
+  for i in range(len(Workshop_List)):
+    for j in range(len(Workshop_List[i])) :
+      if ((i != j) and (Workshop_List[i][1] == Workshop_List[j][1])):
+        if((Workshop_List[i][3] <= Workshop_List[j][4]) and (Workshop_List[i][3] >= Workshop_List[j][3])):
+          return False
+  return True
+
+
+def checkCredits(Workshop_List):
+  count = 0
+  for i in range(len(Workshop_List)) :
+    count += Workshop_List[i][2]
+  return count >= 5
