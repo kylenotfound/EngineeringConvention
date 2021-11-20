1a56fbfd5579862535e7846b93132f2ec81d780d
Kyle <55156513+kylenotfound@users.noreply.github.com>
2021-11-20 13:00:57 -0500

    update to hw

--- Prolog/homework.pl
+++ Prolog/homework.pl
@@ -1,20 +1,21 @@
 certificate(Workshop_List) :-
-    dates(), credits(). %if dates and credits is true then true
+    credits(Workshop_List, C). %if dates and credits is true then true
 
+dates([]).
 dates([H | T]) :-
+    conflict(H, T),
+    write(H),
+    dates(T).
 
+
+%check the date
+conflict([NumA, DayA, CredA, StartA, StopA], []) :- write('yay'), nl.
+
+conflict([NumA, DayA, CredA, StartA, StopA], [[NumB, DayB, CredB, StartB, StopB] | T]) :-
+    DayA \== DayB, write('wow'), nl, conflict([NumA, DayA, CredA, StartA, StopA], T).
+
+credits([ ], 0).
 credits([H | T], Sum) :-
-    credits(T, TSum).
-    Sum is H + TSum.
-
-
-nth0_2(Row, Column, List, Element) :-
-    nth0(Row, List, SubList),
-    nth0(Column, SubList, Element).
-
-
-first([E|,_,_,_,_], E).
-second([_,E|,_,_,_], E).
-third([_,_,E|,_,_], E).
-fourth([_,_,_,E|,_], E).
-fifth([_,_,_,_,E|], E).
+    credits(T, TSum),
+    nth0(2, H, E),
+    Sum is E + TSum.
