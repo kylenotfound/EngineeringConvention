f937f909f1ec47b07b7bc0d537941ca488433842
Kyle <55156513+kylenotfound@users.noreply.github.com>
2021-11-22 16:09:51 -0500

    finish pl homework

--- Prolog/homework.pl
+++ Prolog/homework.pl
@@ -1,21 +1,37 @@
-certificate(Workshop_List) :-
-    credits(Workshop_List, C). %if dates and credits is true then true
+%Kyle Evangelisto
 
+%main entry point checks to see if the credits are satisfied and check the dates
+certificate(List) :-
+    credits(List, Total),
+    Total >= 5, dates(List).
+
+%sum the credits from each list in the master list recursively
+credits([ ], 0).
+credits([[_, _, E | _ ] | T], Sum) :-
+    credits(T, TSum),
+    Sum is E + TSum.
+
+%iterate through all of the dates and check to see if there are any conflicts
 dates([]).
 dates([H | T]) :-
     conflict(H, T),
-    write(H),
     dates(T).
 
-
 %check the date
-conflict([NumA, DayA, CredA, StartA, StopA], []) :- write('yay'), nl.
+conflict([_, _, _, _, _], []).
 
-conflict([NumA, DayA, CredA, StartA, StopA], [[NumB, DayB, CredB, StartB, StopB] | T]) :-
-    DayA \== DayB, write('wow'), nl, conflict([NumA, DayA, CredA, StartA, StopA], T).
+%check the dates in question are not the same
+conflict([NumA, DayA, CredA, StartA, StopA], [[_, DayB, _, _, _] | T]) :-
+    DayA \== DayB, conflict([NumA, DayA, CredA, StartA, StopA], T).
 
-credits([ ], 0).
-credits([H | T], Sum) :-
-    credits(T, TSum),
-    nth0(2, H, E),
-    Sum is E + TSum.
+%check the start and stop times do not violate each other
+conflict([NumA, DayA, CredA, StartA, StopA], [[_, _, _, StartB, StopB] | T]) :-
+    StartA > StopB, StartB > StopA, conflict([NumA, DayA, CredA, StartA, StopA], T).
+
+%check the start and stop times do not violare each other
+conflict([NumA, DayA, CredA, StartA, StopA], [[_, _, _, _, StopB] | T]) :-
+    StartA > StopB, conflict([NumA, DayA, CredA, StartA, StopA], T).
+
+%check the start and stop times do not violate each other
+conflict([NumA, DayA, CredA, StartA, StopA], [[_, _, _, StartB, _] | T]) :-
+    StartB > StopA, conflict([NumA, DayA, CredA, StartA, StopA], T).
